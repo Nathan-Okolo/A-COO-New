496a95042938369f42d41aa778d62a920c6115c9
Okolo Nathan <okolonathanchinedu1@gmail.com>
2023-08-16 04:47:28 +0100

    new

--- js/main.js
+++ js/main.js
@@ -173,3 +173,15 @@
     
 })(jQuery);
 
+$("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
+  });
+  
+  $('.menu-toggle').click(function(){
+     $(".nav").toggleClass("mobile-nav");
+     $(this).toggleClass("is-active");
+  });
+  
+
